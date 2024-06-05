43a842d7c43de6cee3b8172bfa84f81c7a4aacc5
Student <Student@DESKTOP-E2HDGNE>
2024-06-05 15:11:03 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hassan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
